a3b480f5f9e8300cca7386f63c3ce163f6ea651d
Vladimir Drozdik <vladimir.drozdik@gmail.com>
2022-10-26 22:48:14 +0300

    first stuff

---
+++ NOTES.md
@@ -0,0 +1,3 @@
+enter time when last ate
+if 3 hours passed show 'it is time'
+else show 'it is not time yet'
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'is-it-time';
+  isTime: boolean = false;
+  lastTime: string | undefined;
+
+  evaluateIfItIsTime() {
+    if (!this.lastTime) {
+      console.log("last time undefined, evaluated false")
+      this.isTime = false;
+      return;
+    }
+    const date = new Date()
+    date.setHours(parseInt(this.lastTime.split(":")[0]))
+    date.setMinutes(parseInt(this.lastTime.split(":")[1]))
+    const threeHoursSinceLastTime = this.addHours(3, date)
+    this.isTime = new Date() > threeHoursSinceLastTime
+    console.log(`last time defined ${this.lastTime}, evaluated ${this.isTime}`)
+  }
+
+  addHours(numOfHours: number, date = new Date()) {
+    date.setTime(date.getTime() + numOfHours * 60 * 60 * 1000);
+
+    return date;
+  }
 }
